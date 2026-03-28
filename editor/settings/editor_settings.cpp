bceb01721c8532fe97fc914bdd9c67da0f2fe4d2
juanjoseospinabarbosa67-coder <juanjoseospinabarbosa67@gmail.com>
2026-03-27 21:33:54 -0500

    Update project name and copyright information

--- editor/settings/editor_settings.cpp
+++ editor/settings/editor_settings.cpp
@@ -2,10 +2,10 @@
 /*  editor_settings.cpp                                                   */
 /**************************************************************************/
 /*                         This file is part of:                          */
-/*                             GODOT ENGINE                               */
-/*                        https://godotengine.org                         */
+/*                             JJOB ENGINE                               */
+/*                        https://JJOB ENGINE.org                         */
 /**************************************************************************/
-/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2014-present JJOB ENGINE contributors (see AUTHORS.md). */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
 /*                                                                        */
 /* Permission is hereby granted, free of charge, to any person obtaining  */
